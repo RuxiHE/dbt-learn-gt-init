31ef0838cfd2e860eab5789759738f7975cdfe86
RuxiHE <B00810860@essec.edu>
2025-12-13 12:50:21 +0100

    module source.

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,22 @@
-select 
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status as status,
-    amount/100 as amount,
-    created as date_creation_payment,
-from raw.stripe.payment
+with source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        -- amount stored in cent, convert it into dollar.
+        amount / 100 as amount,
+        created as date_creation_payment
+
+    from source
+
+)
+
+select * from renamed
