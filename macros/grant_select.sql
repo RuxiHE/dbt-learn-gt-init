cb5fa9defbd278dc4a25684906f10829cfc7212f
RuxiHE <B00810860@essec.edu>
2026-01-21 14:33:00 +0800

    macro exercises on clean stale models and grant select.

---
+++ macros/grant_select.sql
@@ -0,0 +1,14 @@
+{% macro grant_select(schema=target.schema, role=target.role) %}
+
+    {% set sql %}
+        grant usage on schema {{schema}} to role {{role}}; 
+        grant select on all tables in schema {{schema}} to role {{role}}; 
+        grant select on all views in schema {{schema}} to role {{role}};
+    {% endset %}
+
+    {{log('Granting select on all the tables and views in schema ' ~ schema ~' to role ' ~ role)}}
+
+    {% do run_query(sql) %}
+
+    {{ log('Privileges granted.')}}
+{% endmacro %}
